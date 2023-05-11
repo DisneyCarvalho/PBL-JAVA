86e4ffad58fed60151e1220f13ba297fc583f65b
Rafael Tosta <rafa.ecomp@gmail.com>
2023-05-11 16:42:18 -0300

    :sparkles: fear: Suite de testes
    Execução de todos os testes de uma única vez

--- FBL/src/main/java/module-info.java
+++ FBL/src/main/java/module-info.java
@@ -1,7 +1,20 @@
 module com.example.fbl {
     requires javafx.controls;
     requires javafx.fxml;
-            
+
+
+    opens com.example.fbl.model;
+
+    opens dao.Cliente;
+    opens dao.CompComp;
+    opens dao.Instalacao;
+    opens dao.Limpeza;
+    opens dao.Montagem;
+    opens dao.OrdenServico;
+    opens dao.OutroComp;
+    opens dao.Tecnic;
+
+
                             
     opens com.example.fbl to javafx.fxml;
     exports com.example.fbl;
---
+++ FBL/src/test/java/GATestSuit.java
@@ -0,0 +1,10 @@
+import org.junit.platform.suite.api.*;
+
+@SelectPackages(
+        {"com.example.fbl.model",
+                "dao"
+        }
+)
+@Suite
+public class GATestSuit {
+}
